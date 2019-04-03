efd46c2d451d85a9936dde9a86974fe31def3c3f
pirainogi <pirainogi@gmail.com>
2019-04-03 11:16:46 -0400

    view btn

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -12,7 +12,7 @@ const UserInfo = (props) => {
         <p> <b>Name:</b> {props.user.name}<br></br><br></br>
         <b>Bio:</b> {props.user.bio}<br></br>
         <b>Allergies:</b> {props.user.allergies}<br></br>
-        <b>Diet:</b> {props.user.diet}</p><br></br><br></br>
+        <b>Diet:</b> {props.user.diet}</p><br></br>
         {props.user.recipes.length === undefined ?
           <p><b>RecipeBox:</b> 0 Saved Recipes</p> :
           <p><b>RecipeBox:</b> {props.user.recipes.length} Saved Recipes</p>
