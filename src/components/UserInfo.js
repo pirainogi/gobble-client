ff6a29699aeb4633202cf9469d57ff0f721268fe
pirainogi <pirainogi@gmail.com>
2019-04-04 06:35:40 -0400

    header FINALLY

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -7,12 +7,12 @@ const UserInfo = (props) => {
 
   return (
     <div className="user-info">
-      <img src={props.user.profile_pic} alt="user profile"></img>
+      <img src={props.user.profile_pic} alt="user profile"></img> 
       <div className="details">
         <p> <b>Name:</b> {props.user.name}<br></br><br></br>
         <b>Bio:</b> {props.user.bio}<br></br>
         <b>Allergies:</b> {props.user.allergies}<br></br>
-        <b>Diet:</b> {props.user.diet}</p><br></br>
+        <b>Diet:</b> {props.user.diet}</p>
         {props.user.recipes.length === undefined ?
           <p><b>RecipeBox:</b> 0 Saved Recipes</p> :
           <p><b>RecipeBox:</b> {props.user.recipes.length} Saved Recipes</p>
