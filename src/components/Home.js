226d2d574b79efb52e98dd6ba373208828bf4e4e
pirainogi <pirainogi@gmail.com>
2019-04-04 09:46:59 -0400

    details

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div className="home">
       <h1>search and save recipes. </h1>
-      <h1>create a fully planned calendar.</h1>
+      <h1>create a fully planned meal-prepping calendar.</h1>
       <h1>never worry about <br></br>what the <i>fork</i> is for dinner again.</h1><br></br>
       <h3>join gobble today to get started...</h3>
     </div>
