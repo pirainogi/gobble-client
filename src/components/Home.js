ee175d3d402657ee543f3ffa794b7a4bc8332856
pirainogi <pirainogi@gmail.com>
2019-04-04 05:51:36 -0400

    home pg complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,11 @@ const Home = () => {
 
   return (
     <div className="home">
-      <h1> HOME</h1>
+
+      <h1>search and save recipes. </h1>
+      <h1>create a fully planned calendar.</h1>
+      <h1>never worry about <br></br>what the <i>fork</i> is for dinner again.</h1><br></br>
+      <h3>join gobble today to get started...</h3>
     </div>
   )
 }
