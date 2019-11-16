363348a1e82fc3a9e3b8e7f25f3152e3300f76c3
pirainogi <pirainogi@gmail.com>
2019-11-15 21:11:50 -0500

    comments calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,11 +3,14 @@ import BigCalendar from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
 import 'moment-timezone';
+//importing react and all libraries
 import '../css/Calendar.css';
+//importing the CSS for this component
+
 moment.locale('en');
 moment.tz.setDefault("America/New_York");
 const localizer = BigCalendar.momentLocalizer(moment);
-
+//set the locale and timezone to NYC and passing it to BigCal library
 
 class Calendar extends Component {
 
@@ -15,22 +18,20 @@ class Calendar extends Component {
     events: [],
   }
 
+  //on mount, map over all the events that have been passed down as props and create event start and end times and set those to the local state in this component
   componentDidMount() {
-    console.log(this.props.events);
     let calEvents = this.props.events.map( event => {
         event.eventEnd = new Date(event.eventEnd)
         event.eventStart = new Date(event.eventStart)
         return event
       })
-    console.log(calEvents);
     this.setState({
       events: calEvents
     })
   }
 
-
+  //conditionally render the calendar upon the props being passed down 
   render(){
-    console.log('calendar props', this.props, 'state', this.state)
     return (
       <div className='calendar'>
       {this.props.events[0].id ?
