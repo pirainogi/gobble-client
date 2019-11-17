80f476d8d537660c4b430691554245d1f5976a12
pirainogi <pirainogi@gmail.com>
2019-11-17 14:47:53 -0500

    comments dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import '../css/Dashboard.css';
-// import Calendar from './Calendar'
-import RecipeBoxPreview from './RecipeBoxPreview'
-// import Header from './Header'
-// import Footer from './Footer'
+//this was never implemented
 
-const Dashboard = (props) => {
-// console.log(props);
-
-  let generateRecipeBoxPreviews = () => {
-    return props.currentRecipebox.map(recipe => {
-      return (
-        <RecipeBoxPreview
-          key={recipe.id}
-          recipe={recipe}
-          selectRecipePreviewForShow={props.selectRecipePreviewForShow}
-        />
-      )
-    })
-  }
-
-  return (
-    <div className="dashboard">
-      <div>
-        <h1> dashboard </h1>
-        {generateRecipeBoxPreviews()}
-      </div>
-    </div>
-  )
-}
-
-export default Dashboard;
-
-// <Calendar />
+// import React from 'react';
+// import '../css/Dashboard.css';
+// import RecipeBoxPreview from './RecipeBoxPreview'
+//
+//
+// const Dashboard = (props) => {
+//
+//   let generateRecipeBoxPreviews = () => {
+//     return props.currentRecipebox.map(recipe => {
+//       return (
+//         <RecipeBoxPreview
+//           key={recipe.id}
+//           recipe={recipe}
+//           selectRecipePreviewForShow={props.selectRecipePreviewForShow}
+//         />
+//       )
+//     })
+//   }
+//
+//   return (
+//     <div className="dashboard">
+//       <div>
+//         <h1> dashboard </h1>
+//         {generateRecipeBoxPreviews()}
+//       </div>
+//     </div>
+//   )
+// }
+//
+// export default Dashboard;
