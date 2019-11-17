95a8791cff358c5fe7ccc01690be0489da47f05a
pirainogi <pirainogi@gmail.com>
2019-11-17 14:58:42 -0500

    comments event form

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -3,47 +3,58 @@ import '../css/EventForm.css';
 
 class EventForm extends Component {
 
+  //state for a controlled form
   state = {
     date: {},
     startTime: {},
     endTime: {},
-    eventStart: {},
-    eventEnd: {},
     recipeName: ''
   }
 
+  //on change of the form below
   handleChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value
     })
   }
 
+  //on pushing submit, prevent the auto-refresh, create the stringified dates that BigCal needs, then call the createEvent function that's been passed down as a prop and pass in the necessary arguments
   handleSubmit = (e) => {
     e.preventDefault()
-    console.log('pushing submit button and logging state', this.state);
     let stringifiedStart = this.state.date + "T" + this.state.startTime + ":00.000-05:00"
     let stringifiedEnd = this.state.date + "T" + this.state.endTime + ":00.000-05:00"
-    // console.log(stringifiedStart, stringifiedEnd, this.state.recipeName);
     this.props.createEvent(stringifiedStart, stringifiedEnd, this.state.recipeName)
   }
 
   render(){
-    console.log(this.props);
     return (
       <div className='form'>
-
           <form>
-            <label><b>Select a Date:</b></label><br></br>
-            <input type="date" id="event-date" name="date" min="2019-04-03" max="2019-12-31" onChange={this.handleChange} required></input><br></br>
-            <label><b>Select a Start Time:</b></label><br></br>
-            <input type="time" id="event-start" name="startTime" min="00:00" max="23:00" onChange={this.handleChange} required></input><br></br>
-            <label><b>Select an End Time:</b></label><br></br>
-            <input type="time" id="event-end" name="endTime" min="01:00" max="24:00" onChange={this.handleChange} required></input><br></br><br></br>
-            <label><b>Recipe Name:</b></label><br></br>
-            <input type="text" id="recipeName" name="recipeName" placeholder={this.props.recipeForEvent} onChange={this.handleChange} required></input><br></br><br></br>
+            <label>
+              <b>Select a Date:</b>
+            </label>
+            <br></br>
+            <input type="date" id="event-date" name="date" min="2019-04-03" max="2019-12-31" onChange={this.handleChange} required></input>
+            <br></br>
+            <label>
+              <b>Select a Start Time:</b>
+            </label>
+            <br></br>
+            <input type="time" id="event-start" name="startTime" min="00:00" max="23:00" onChange={this.handleChange} required></input>
+            <br></br>
+            <label>
+              <b>Select an End Time:</b>
+            </label>
+            <br></br>
+            <input type="time" id="event-end" name="endTime" min="01:00" max="24:00" onChange={this.handleChange} required></input>
+            <br></br><br></br>
+            <label>
+              <b>Recipe Name:</b>
+            </label><br></br>
+            <input type="text" id="recipeName" name="recipeName" placeholder={this.props.recipeForEvent} onChange={this.handleChange} required></input>
+            <br></br><br></br>
             <button onClick={this.handleSubmit}>Add to Calendar</button>
           </form>
-
       </div>
     )
   }
