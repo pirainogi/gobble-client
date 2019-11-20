62e9d7211ca586d6b43d34c84299107aa64f5f15
pirainogi <pirainogi@gmail.com>
2019-11-20 11:46:47 -0500

    search icons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,6 +11,22 @@ class SearchBar extends Component {
           placeholder='Search for Recipes...'
           onChange={this.props.filterRecipesBySearchInput}
         />
+        <div className="recipe-icons">
+          <img className="iconboolean" src={process.env.PUBLIC_URL + '/gf-4c.jpg'} alt='gluten-free icon'></img>
+
+          <img className="iconboolean" src={process.env.PUBLIC_URL + '/df-4c.jpg'} alt='dairy-free icon'></img>
+
+          <img className="iconboolean" src={process.env.PUBLIC_URL + '/keto-4c.jpg'} alt='keto icon'></img>
+
+          <img className="iconboolean" src={process.env.PUBLIC_URL + '/whole30_4c.jpg'} alt='whole30 icon'></img>
+
+          <img className="iconboolean" src={process.env.PUBLIC_URL + '/veg-4c.png'} alt='vegetarian icon'></img>
+
+          <img className="iconboolean" src={process.env.PUBLIC_URL + '/vegan-4c.jpeg'} alt='vegan icon'></img>
+
+          <img className="iconboolean" src={process.env.PUBLIC_URL + '/healthy-4c.jpg'} alt='healthy icon'></img>
+        </div>
+
       </form>
     )
   }
