ebbdc379fa5e4e49595475028d84c5494f347c70
pirainogi <pirainogi@gmail.com>
2019-11-28 09:38:23 -0500

    state change is working for input and for button click

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -46,6 +46,52 @@ class SearchContainer extends Component {
     })
   }
 
+  changeDietaryRestrictions = (e) => {
+    console.log('changing the diet to:', e.target.id)
+    this.setState({
+      restrictions: {
+        ...this.state.restrictions,
+        [e.target.id]: !this.state.restrictions[e.target.id]
+      }
+    })
+  }
+
+  changeSearchValue = (e) => {
+    console.log('changing search value');
+    this.setState({
+      searchValue: e.target.value
+    })
+  }
+
+  clickListener = (e) => {
+    if(e.target.dataset.id === "input"){
+      console.log('search value');
+      this.changeSearchValue(e)
+    } else if(e.target.dataset.id === "button") {
+      console.log('diet restriction');
+      this.changeDietaryRestrictions(e)
+    } else {
+
+    }
+  }
+
+  render(){
+    console.log('state:', this.state.searchValue, this.state.restrictions)
+    return (
+      <div className="search-container">
+        <SearchBar clickListener={this.clickListener} vegan={this.state.restrictions.vegan} vegetarian={this.state.restrictions.vegetarian} glutenfree={this.state.restrictions.glutenfree} dairyfree={this.state.restrictions.dairyfree}/>
+        <div className='seached-recipes-container'>
+          {this.generateRecipePreview()}
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default (SearchContainer);
+
+
   //if the input field isn't empty, then filter all of the recipes and return those that match, set the returned value in state so they render in the component; otherwise just render all the recipes
   // filterRecipesByInput = () => {
   //   let searchedRecipes
@@ -108,111 +154,70 @@ class SearchContainer extends Component {
   //     recipesReturnedFromSearch: searchedRecipes
   //   })
   // }
-
-  filterRecipesByInput = () => {
-    const restrictions = []
-    for(let diet in this.state.restrictions){
-      if(this.state.restrictions[diet]){
-        restrictions.push(diet)
-      }
-    }
-    return restrictions
-  }
-
-  multiDietFilter = (recipes, restrictions) => {
-    // console.log(recipes, 'array of diets:', restrictions)
-    return recipes.filter(recipe => {
-      return restrictions.every(diet => {
-        console.log(diet, recipe)
-        // recipes.includes(recipe[diet] === true)
-        // if(!restrictions[diet].length) {
-        //   return true
-        // }
-        // return restrictions[diet].includes(recipe[diet] === true)
-      })
-    })
-  }
-
-  searchRecipes = () => {
-    console.log('hitting search recipes','current num of recipes:', this.state.recipesReturnedFromSearch.length, this.state.searchValue);
-    // const filteredRecipes = this.multiDietFilter(this.state.recipesReturnedFromSearch, this.filterRecipesByInput())
-    let fr, starter
-    starter = this.state.recipesReturnedFromSearch
-    fr = starter.filter(recipe => {
-      const lowercaseRecipe = recipe.name.toLowerCase()
-      const lowerCaseFilter = this.state.searchValue.toLowerCase()
-      return lowercaseRecipe.includes(lowerCaseFilter)
-    })
-    this.setState({
-      recipesReturnedFromSearch: fr
-    }, console.log(this.state.recipesReturnedFromSearch.length, this.state.searchValue))
-  }
-
-  changeDietaryRestrictions = (e) => {
-    // console.log('changing the diet to:', e.target.id)
-    this.setState({
-      restrictions: {
-        ...this.state.restrictions,
-        [e.target.id]: !this.state.restrictions[e.target.id]
-      }
-    }, this.searchRecipes)
-  }
-
-  changeSearchValue = (e) => {
-    // console.log('changing search value');
-    this.setState({
-      searchValue: e.target.value
-    }, this.searchRecipes)
-  }
-
-  //this function will change the local state to reflect the dietary filters
-  filterRecipesByDietaryRestriction = () => {
-    console.log('hitting the method')
-    let currentRecipes, filteredRecipes
-    currentRecipes=this.state.recipesReturnedFromSearch
-    filteredRecipes = currentRecipes.filter(recipe => {
-      return recipe.vegan === true
-    })
-    this.setState({
-      recipesReturnedFromSearch: filteredRecipes
-    })
-  }
-
-  filterRecipesBySearchInput = () => {
-    let currentRecipes, filteredRecipes
-    currentRecipes = this.state.recipesReturnedFromSearch
-    filteredRecipes = currentRecipes.filter(recipe => {
-      const lowercaseRecipe = recipe.name.toLowerCase()
-      const lowerCaseFilter = this.state.searchValue.toLowerCase()
-      return lowercaseRecipe.includes(lowerCaseFilter)
-    })
-    this.setState({
-      recipesReturnedFromSearch: filteredRecipes
-    })
-  }
-
-  clickListener = (e) => {
-    if(e.target.dataset.id === "input"){
-      this.changeSearchValue(e)
-    } else if(e.target.dataset.id === "button") {
-      this.changeDietaryRestrictions(e)
-    } else {
-
-    }
-  }
-
-  render(){
-    // console.log('search val:', this.state.searchValue)
-    return (
-      <div className="search-container">
-        <SearchBar clickListener={this.clickListener} vegan={this.state.restrictions.vegan} vegetarian={this.state.restrictions.vegetarian} glutenfree={this.state.restrictions.glutenfree} dairyfree={this.state.restrictions.dairyfree}/>
-        <div className='seached-recipes-container'>
-          {this.generateRecipePreview()}
-        </div>
-      </div>
-    )
-  }
-
-}
-
-export default (SearchContainer);
+  //
+  // filterRecipesByInput = () => {
+  //   const restrictions = []
+  //   for(let diet in this.state.restrictions){
+  //     if(this.state.restrictions[diet]){
+  //       restrictions.push(diet)
+  //     }
+  //   }
+  //   return restrictions
+  // }
+  //
+  // multiDietFilter = (recipes, restrictions) => {
+  //   // console.log(recipes, 'array of diets:', restrictions)
+  //   return recipes.filter(recipe => {
+  //     return restrictions.every(diet => {
+  //       console.log(diet, recipe)
+  //       // recipes.includes(recipe[diet] === true)
+  //       // if(!restrictions[diet].length) {
+  //       //   return true
+  //       // }
+  //       // return restrictions[diet].includes(recipe[diet] === true)
+  //     })
+  //   })
+  // }
+  //
+  // searchRecipes = () => {
+  //   console.log('hitting search recipes','current num of recipes:', this.state.recipesReturnedFromSearch.length, this.state.searchValue);
+  //   // const filteredRecipes = this.multiDietFilter(this.state.recipesReturnedFromSearch, this.filterRecipesByInput())
+  //   let fr, starter
+  //   starter = this.state.recipesReturnedFromSearch
+  //   fr = starter.filter(recipe => {
+  //     const lowercaseRecipe = recipe.name.toLowerCase()
+  //     const lowerCaseFilter = this.state.searchValue.toLowerCase()
+  //     return lowercaseRecipe.includes(lowerCaseFilter)
+  //   })
+  //   this.setState({
+  //     recipesReturnedFromSearch: fr
+  //   }, console.log(this.state.recipesReturnedFromSearch.length, this.state.searchValue))
+  // }
+  //
+  //
+  // //this function will change the local state to reflect the dietary filters
+  // filterRecipesByDietaryRestriction = () => {
+  //   console.log('hitting the method')
+  //   let currentRecipes, filteredRecipes
+  //   currentRecipes=this.state.recipesReturnedFromSearch
+  //   filteredRecipes = currentRecipes.filter(recipe => {
+  //     return recipe.vegan === true
+  //   })
+  //   this.setState({
+  //     recipesReturnedFromSearch: filteredRecipes
+  //   })
+  // }
+  //
+  // filterRecipesBySearchInput = () => {
+  //   let currentRecipes, filteredRecipes
+  //   currentRecipes = this.state.recipesReturnedFromSearch
+  //   filteredRecipes = currentRecipes.filter(recipe => {
+  //     const lowercaseRecipe = recipe.name.toLowerCase()
+  //     const lowerCaseFilter = this.state.searchValue.toLowerCase()
+  //     return lowercaseRecipe.includes(lowerCaseFilter)
+  //   })
+  //   this.setState({
+  //     recipesReturnedFromSearch: filteredRecipes
+  //   })
+  // }
+  //
