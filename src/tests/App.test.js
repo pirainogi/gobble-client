ad8d7368c6730aed440c2b3b239c732635b4d0c4
pirainogi <pirainogi@gmail.com>
2019-12-12 13:53:07 -0500

    it actually runs a test

---
+++ src/tests/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { BrowserRouter as Router } from "react-router-dom";
+import {render} from '@testing-library/react'
+import App from '../App'
+
+it('renders without crashing', () => {
+const {getByText} = render(<Router><App /></Router>)
+})
